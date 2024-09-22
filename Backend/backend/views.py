dcbb5308079dc2c136d5e671f00b8d9461f451a5
rudra990 <rudraashutosh777@gmail.com>
2024-09-22 15:51:33 +0530

    First commit

---
+++ Backend/backend/views.py
@@ -0,0 +1,6 @@
+## added this file afte debugging with chatgpt
+
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Welcome to the Rudra's LMS Home Page!")
---
+++ Backend/userauths/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+from userauths.models import User, Profile
+
+class ProfileAdmin(admin.ModelAdmin):
+  list_display = ['user','full_name', 'date']
+
+
+
+admin.site.register(User)
+admin.site.register(Profile, ProfileAdmin)
+# Register your models here.
