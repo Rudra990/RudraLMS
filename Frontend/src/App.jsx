0040628edfd2756242169264d1c4b68993236fc8
rudra990 <rudraashutosh777@gmail.com>
2025-02-10 18:34:30 +0530

    app.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 import MainWrapper from "./layouts/MainWrapper";
 import PrivateRoute from "./layouts/PrivateRoute";
@@ -7,6 +8,9 @@ import Login from "../src/views/auth/Login";
 import Logout from "./views/auth/Logout";
 import ForgotPassword from "./views/auth/ForgotPassword";
 import CreateNewPassword from "./views/auth/CreateNewPassword";
+import Index from "./views/base/Index";
+
+
 function App() {
   return (
     <BrowserRouter>
@@ -18,6 +22,15 @@ function App() {
           <Route path="/ForgotPassword/" element={<ForgotPassword />} />
           <Route path="/CreateNewPassword/" element={<CreateNewPassword />} />
           
+           {/* Base Routes */}
+           <Route path="/" element={<Index />} />
+           {/* <Route path="/course-detail/:slug/" element={<CourseDetail />} />
+           <Route path="/cart/" element={<Cart />} />
+           <Route path="/checkout/:order_oid/" element={<Checkout />} />
+           <Route path="/payment-success/:order_oid/" element={<Success />} />
+           <Route path="/search/" element={<Search />} /> */}
+
+
         </Routes>
       </MainWrapper>
     </BrowserRouter>
@@ -25,3 +38,6 @@ function App() {
 }
 
 export default App;
+
+
+
